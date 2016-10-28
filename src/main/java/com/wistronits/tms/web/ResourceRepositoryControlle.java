697c78dfe94acfc61968383e15c8a9144438fb24
mingpan <WH1510051@PC-WH1510051.wh.wistronits.com>
2016-10-28 16:01:35 +0800

    add resume

--- src/main/java/com/wistronits/tms/web/ResourceRepositoryControlle.java
+++ src/main/java/com/wistronits/tms/web/ResourceRepositoryControlle.java
@@ -1,10 +1,19 @@
 package com.wistronits.tms.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/Resource")
 public class ResourceRepositoryControlle {
+
+	
+	@RequestMapping(value = "/toAdd", method = RequestMethod.POST)
+	  public String toAdd(Model model) {
+
+	    return "/resource/resource_add";
+	  }
 
 }
