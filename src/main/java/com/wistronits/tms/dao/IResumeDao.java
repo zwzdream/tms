534d81f56c1a504491659e77c06c71a69b54f59d
mingpan <WH1510051@PC-WH1510051.wh.wistronits.com>
2016-11-01 17:13:24 +0800

    1

--- src/main/java/com/wistronits/tms/dao/IResumeDao.java
+++ src/main/java/com/wistronits/tms/dao/IResumeDao.java
@@ -1,8 +1,8 @@
 package com.wistronits.tms.dao;
 
-import com.wistronits.tms.entity.ResumeDto;
+import com.wistronits.tms.entity.ResumeBean;
 
 public interface IResumeDao {
 
-	 public void saveResume(ResumeDto rDto);
+	 public void saveResume(ResumeBean rDto);
 }
