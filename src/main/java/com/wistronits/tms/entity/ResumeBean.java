0ad2959a6889fae67f6588bffddca0905b81505d
mingpan <WH1510051@PC-WH1510051.wh.wistronits.com>
2016-11-09 15:28:31 +0800

    增加属性

--- src/main/java/com/wistronits/tms/entity/ResumeBean.java
+++ src/main/java/com/wistronits/tms/entity/ResumeBean.java
@@ -1,13 +1,80 @@
 package com.wistronits.tms.entity;
 
 import java.io.Serializable;
+import java.util.Date;
+
 
 public class ResumeBean implements Serializable {
 		
 	private int id;
 	private String firstName;
 	private String lastName;
+	private Date birth;
+	private String gender;
+	private String mobile;
+	private Date starts;
+	private String email;
+	private String residency;
+	private String education;
+	private String workExp;
+	private String projectExp;
+	 
 	
+	public String getEducation() {
+		return education;
+	}
+	public void setEducation(String education) {
+		this.education = education;
+	}
+	public String getWorkExp() {
+		return workExp;
+	}
+	public void setWorkExp(String workExp) {
+		this.workExp = workExp;
+	}
+	public String getProjectExp() {
+		return projectExp;
+	}
+	public void setProjectExp(String projectExp) {
+		this.projectExp = projectExp;
+	}
+	
+	public String getResidency() {
+		return residency;
+	}
+	public void setResidency(String residency) {
+		this.residency = residency;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Date getStarts() {
+		return starts;
+	}
+	public void setStarts(Date starts) {
+		this.starts = starts;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public Date getBirth() {
+		return birth;
+	}
+	public void setBirth(Date birth) {
+		this.birth = birth;
+	}
 	public int getId() {
 		return id;
 	}
